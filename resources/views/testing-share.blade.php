a89e4ea94cbee02fb9100fd93382bdf470ed77bb
yysrll <yusril.uzumaki@gmail.com>
2023-04-05 14:22:16 +0800

    add catch to navigator

--- resources/views/testing-share.blade.php
+++ resources/views/testing-share.blade.php
@@ -19,12 +19,11 @@
 
 
             <div class="flex my-4 mx-4 md:mx-0">
-                <a
-                    href="javascript:void(0)"
+                <button
                     onclick="shareImage()"
                     class="text-center px-6 py-4 ml-4 border border-transparent rounded-md font-semibold text-xs text-white tracking-widest hover:bg-blue-300 focus:bg-blue-300 active:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
                     style="background-color: rgb(32 110 168);"
-                    >Bagikan</a>
+                    >Bagikan</button>
             </div>
 
         </div>
@@ -41,8 +40,10 @@
 
         function shareImage() {
             // Get the image source URL
+            console.log('share start')
             const image = document.getElementById('myImage');
 
+            console.log('create share data')
             const shareData = {
                 title: 'Share Image', // Optional title
                 files: [image.src], // The image file to share
@@ -50,13 +51,20 @@
             };
 
             // Check if the share API is available in the browser
+
+            console.log('send data')
             if (navigator.share) {
                 // Use the share API to share the image
-                navigator.share(shareData);
+                navigator
+                .share(shareData)
+                .then(() => console.log("Shared!"))
+                .catch(err => console.error(err));;
             } else {
                 // Use a fallback method to share the image
                 alert('Share API is not supported in this browser');
             }
+
+            console.log('share end')
         }
     </script>
 </x-app-layout>
