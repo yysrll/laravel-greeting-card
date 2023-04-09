773a6c49f43698566466d35304ffb8f2d099659c
yysrll <yusril.uzumaki@gmail.com>
2023-04-09 21:51:31 +0800

    fix crop photo

--- resources/views/pages/form/form.blade.php
+++ resources/views/pages/form/form.blade.php
@@ -4,7 +4,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.css" integrity="sha512-C4k/QrN4udgZnXStNFS5osxdhVECWyhMsK1pnlk+LkC7yJGCqoYxW4mH3/ZXLweODyzolwdWSqmmadudSHMRLA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     </x-slot>
 
-    <div class="relative">
+    <div class="relative" id="content">
         <div class="grid grid-cols-1 md:grid-cols-3 bg-white md:bg-gray-200">
             <div class="col-span-1">
 
@@ -96,32 +96,42 @@
                 <div class="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-[#3CA949]"></div>
             </div>
         </div>
+    </div>
 
+    <div class="relative hidden" id="my-modal">
+        <div class="grid grid-cols-1 md:grid-cols-3 bg-white md:bg-gray-200">
+            <div class="col-span-1">
 
-        {{-- Modal --}}
-        <div
-            class="fixed hidden inset-0 bg-gray-600 bg-opacity-50 w-full pb-12"
-            id="my-modal"
-        >
-            <!--modal content-->
-            <div
-            class="relative w-full h-full overflow-y-scroll md:w-1/2 top-12 mb-12 mx-auto p-5 border shadow-lg rounded-md bg-white"
-            >
-                <div class="flex justify-center mt-3 text-center">
-                    <div class="items-center px-2 py-2">
-                        <img class="max-h-screen" id="imageCrop" src="{{ url('images/template.jpeg')}}">
+            </div>
+            <div class="bg-white col-span-1 min-h-screen ">
+                <div class="px-4 md:px-10 py-6 bg-[#206EA8] sticky top-0">
+                    <div class="flex justify-end items-center">
                         <button
-                            id="crop-btn"
-                            class="mt-16 px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300"
-                        >
-                            Simpan
+                            id="btnClose"
+                            class="p-2 bg-blue-300/30 rounded-md"
+                            onclick="hideModal()"
+                            >
+                            <svg class="w-4 h-4" fill="#ffffff"  version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 460.775 460.775" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"></path> </g></svg>
                         </button>
                     </div>
                 </div>
+                <div class="flex justify-center pt-8 w-full" id="containerImage">
+                    <img class="w-full" id="imageCrop" src="{{ url('images/template.jpeg')}}">
+                </div>
+                <div class="px-4 md:px-10 py-6 bg-white sticky bottom-0">
+                    <button
+                    id="crop-btn"
+                    class="w-full text-center text-white bg-[#206EA8] px-16 py-4 border border-transparent rounded-md font-semibold text-xs tracking-widest"
+                    >
+                        Terapkan
+                    </button>
+                </div>
+
+            </div>
+            <div class="col-span-1">
+
             </div>
         </div>
-
-
     </div>
 
 
@@ -164,6 +174,7 @@
     {{-- Generate Script --}}
     <script>
         let modal = document.getElementById("my-modal");
+        let content = document.getElementById("content");
         let btn = document.getElementById("open-btn");
         let cropButton = document.getElementById("crop-btn");
 
@@ -174,10 +185,31 @@
         // We want the modal to open when the Open button is clicked
         function showModal() {
             modal.style.display = "block";
+            content.style.display = "none";
             cropper = new Cropper(image, {
+                dragMode: 'move',
                 aspectRatio: 1,
-                viewMode: 3,
+                viewMode: 1,
+                autoCropArea: 1,
+                cropBoxMovable: false,
+                cropBoxResizable: false,
+                toggleDragModeOnDblclick: false,
+                data: {
+                    scaleX: 1, scaleY: 1
+                },
+                ready: function () {
+                    cropper.setCropBoxData({
+                        top: 0,
+                    });
+                },
             });
+        }
+
+        function hideModal() {
+            modal.style.display = "none";
+            content.style.display = "block";
+            cropper.destroy();
+            cropper = null;
         }
 
         document.getElementById("image").onchange = function(e) {
@@ -213,27 +245,24 @@
         // We want the modal to close when the OK button is clicked
         cropButton.onclick = function() {
             canvas = cropper.getCroppedCanvas({
-                width: 160,
-                height: 160,
+                minWidth: 256,
+                minHeight: 256,
+                maxWidth: 4096,
+                maxHeight: 4096,
+                imageSmoothingEnabled: true,
+                imageSmoothingQuality: 'high'
             });
 
             canvas.toBlob(function(blob) {
+
+                imageData = blob;
+                console.log(imageData)
+
                 url = URL.createObjectURL(blob);
                 var reader = new FileReader();
                 reader.readAsDataURL(blob);
                 reader.onloadend = function() {
                     var ImageURL = reader.result;
-
-                    // Split the base64 string in data and contentType
-                    var block = ImageURL.split(";");
-                    // Get the content type of the image
-                    var contentType = block[0].split(":")[1];// In this case "image/gif"
-                    // get the real base64 content of the file
-                    var realData = block[1].split(",")[1];// In this case "R0lGODlhPQBEAPeoAJosM...."
-
-                    // Convert it to a blob to upload
-                    imageData = b64toBlob(realData, contentType);
-                    console.log(imageData)
 
                     // Create a new image element
                     var imgPreview = document.getElementById("preview-image");
@@ -244,15 +273,25 @@
                     document.getElementById("inputLabel").textContent = 'Ubah Gambar';
 
                 }
-            });
+            }, 'image/jpeg', 1);
 
 
             modal.style.display = "none";
+            content.style.display = "block";
             cropper.destroy();
             cropper = null;
         }
 
         $("#generateButton").click(function (event) {
+            if ($('#sender').val() == null || $('#sender').val() == "") {
+                alert('Kolom nama tidak boleh kosong')
+                return
+            }
+            if (imageData == null) {
+                alert('Foto tidak boleh kosong')
+                return
+            }
+
             $('#loading').show()
             console.log('submit')
             //stop submit the form, we will post it manually.
@@ -291,29 +330,5 @@
             });
 
         });
-
-        function b64toBlob(b64Data, contentType, sliceSize) {
-            contentType = contentType || '';
-            sliceSize = sliceSize || 512;
-
-            var byteCharacters = atob(b64Data);
-            var byteArrays = [];
-
-            for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-                var slice = byteCharacters.slice(offset, offset + sliceSize);
-
-                var byteNumbers = new Array(slice.length);
-                for (var i = 0; i < slice.length; i++) {
-                    byteNumbers[i] = slice.charCodeAt(i);
-                }
-
-                var byteArray = new Uint8Array(byteNumbers);
-
-                byteArrays.push(byteArray);
-            }
-
-            var blob = new Blob(byteArrays, {type: contentType});
-            return blob;
-        }
     </script>
 </x-app-layout>
